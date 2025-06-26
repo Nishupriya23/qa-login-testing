87552dd830d49312bac373f20c7b2718f41bd1cf
Nishupriya23 <nishupriya218@gmail.com>
2025-06-26 13:02:26 +0530

    Added Selenium Automation using Java file

---
+++ Selenium Automation/LoginTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+    }
+
+    @Test
+    public void validLoginTest() {
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
+        Assert.assertTrue(driver.getPageSource().contains("Logged In Successfully"));
+    }
+
+    @AfterClass
+    public void teardown() {
+        driver.quit();
+    }
+}
